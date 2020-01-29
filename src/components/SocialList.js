520f1d2262037c4ac3cad898b467119ddb8dd112
kyle1668 <kyledevinobrien1@gmail.com>
2020-01-29 15:28:06 -0800

    updated IG handle

--- src/components/SocialList.js
+++ src/components/SocialList.js
@@ -8,7 +8,7 @@ class SocialList extends React.Component {
 			socials: [
 				'https://www.linkedin.com/in/kyle1668',
 				'https://github.com/Kyle1668',
-				'https://www.instagram.com/kyle_1668/?hl=en',
+				'https://www.instagram.com/kyle.ob_/?hl=en',
 				'https://medium.com/@kyleobrien1668'
 			]
 		};
